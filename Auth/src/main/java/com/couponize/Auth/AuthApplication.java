796010a0af6afb64433269ba7743ba2dc3f38091
Humphreychinedu <mefendja.chinedu@gmail.com>
2018-12-04 19:20:05 +0100

    flyway imtegration

--- Auth/src/main/java/com/couponize/Auth/AuthApplication.java
+++ Auth/src/main/java/com/couponize/Auth/AuthApplication.java
@@ -2,11 +2,11 @@ package com.couponize.Auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 //@ComponentScan("com.couponize.Auth")
 public class AuthApplication {
 

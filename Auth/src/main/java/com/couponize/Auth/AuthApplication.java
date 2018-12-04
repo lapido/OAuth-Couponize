8c37e56fadb222e9cdc29fb2882335ffa8bbba64
lapido <victortemitope95@gmail.com>
2018-12-04 09:31:49 +0100

    Made modification to the location of the main class

--- Auth/src/main/java/com/couponize/Auth/AuthApplication.java
+++ Auth/src/main/java/com/couponize/Auth/AuthApplication.java
@@ -1,11 +1,13 @@
-package com.couponize.Auth.Auth;
+package com.couponize.Auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
 @SpringBootApplication
 @EnableEurekaClient
+//@ComponentScan("com.couponize.Auth")
 public class AuthApplication {
 
 	public static void main(String[] args) {
